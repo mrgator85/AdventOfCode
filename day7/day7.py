9fa5484abfaf115bb4c4f891b6da391c41a47023
Andrew Allgeier <andrew.allgeier@tangramflex.com>
2020-12-08 07:47:38 -0500

    Finsished day 2

--- day7/day7.py
+++ day7/day7.py
@@ -9,7 +9,6 @@ class Bag(object):
         self.contents = contents
         self.gold = self.containsGold()
     def containsGold(self, n = 1):
-        #print(self.contents)
         return 'shiny gold' in [x[0] for x in self.contents] 
 
 class Counter(object):
@@ -28,14 +27,11 @@ def parseRule(line):
             num = int(v.group(0))
             desc = re.split(r'[0-9]+?|bag', r.strip())[1]
             contents.append((desc.strip(), int(num)))
-        
-    #print(contents)
     return Bag(name, contents)
 
 def buildGraph(lines):
     bags = {}
     for l in lines:
-        print(l)
         bag = parseRule(l)
         bags[bag.description] = bag
     return bags
@@ -43,22 +39,31 @@ def buildGraph(lines):
 def findGold(g, visited, node, counter):
     if(node not in visited):
         if(g[node].containsGold()):
-            print('found gold')
             counter.inc()
         visited.append(node)
         for c in g[node].contents:
             findGold(g, visited, c[0], counter)
 
+def CountBags(g, visited, node):
+    if(node not in visited):
+        if len(g[node].contents) == 0:
+            return 1
+        else:
+            t = 1 + sum([ n[1] * CountBags(g, visited, n[0]) for n in g[node].contents])
+            return t
+    visited.append(node)
+
 if __name__ == "__main__":
     with open('input.txt', 'r') as f:
         g = buildGraph(f.readlines())
-        print(g.keys())
         bagCount = 0
         for k in g.keys():
             visited = []
             counter = Counter(0)
             findGold(g, visited, k, counter)
-            #print(counter)
             if(counter.v > 0):
                 bagCount = bagCount + 1
         print(bagCount)
+        counter = Counter()
+        visited = []
+        print(CountBags(g, visited, 'shiny gold')-1) #not sure why the minus 1, is this the shiny gold back itself?
