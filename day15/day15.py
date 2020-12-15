1e8d49d37fd8ce488112ccd8eafed81c7778de76
Andrew Allgeier <andrew.allgeier@tangramflex.com>
2020-12-15 14:14:09 -0500

    finished part 2

--- day15/day15.py
+++ day15/day15.py
@@ -1,15 +1,32 @@
 def findNthSpoken(spoken, n):
     turn = 0
+    lastindx = {}
+    lastnumber = 0
+
     while(turn < n):
         if(turn < len(spoken)):
-            turn = turn + 1
-        elif(turn == len(spoken)):
-            if(spoken[turn-1] in spoken[:turn-1]):
-                spoken.append(turn-1 - findLastIndexOf(spoken[:turn-1], spoken[turn-1]))
+            v = lastnumber
+            #lastindx[spoken[turn]] = turn 
+            lastnumber = spoken[turn]
+            lastindx[v] = turn - 1
+            #print(turn+1)
+            #print(lastnumber)
+            #print('----')
+        else:
+            v = lastnumber
+            if(lastnumber in lastindx.keys()):
+                #print(f"----{turn} --- {lastindx[lastnumber]}")
+                lastnumber = turn - 1 - lastindx[lastnumber]
+                lastindx[v] = turn -1 
             else:
-                spoken.append(0)
-            turn = turn + 1
-    return spoken[turn-1]
+                lastnumber = 0
+                lastindx[v] = turn - 1
+            #print(turn+1)
+            #print(lastnumber)
+            #print('----')
+        
+        turn = turn + 1
+    return lastnumber
 
 def findLastIndexOf(a, v):
     return len(a) - 1 - a[::-1].index(v)
@@ -18,4 +35,4 @@ def findLastIndexOf(a, v):
 if __name__ == "__main__":
     sample_input = [0, 3, 6]
     my_input = [8, 0, 17, 4, 1, 12]
-    print(findNthSpoken(sample_input, 2020))
+    print(findNthSpoken(my_input, 30000000))
