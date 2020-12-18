820438af50c406ad6f354f9b3815922da5f9e5fa
Andrew Allgeier <andrew.allgeier@tangramflex.com>
2020-12-18 13:00:48 -0500

    Finished day 18, cleanup

--- day18/day18.py
+++ day18/day18.py
@@ -1,12 +1,11 @@
 import re
-def evaluate(s):
+def evaluate(prob):
     
     op = '+'
     counter = 0
     c = next(prob, 'end')
     
     while(c != 'end' ):
-        print(f"c: |{c}|") 
         if(c in ['+', '*']):
             op = c 
         elif(c == '('):
@@ -23,14 +22,40 @@ def evaluate(s):
             counter = counter + v
         c = next(prob, 'end')
     return counter
+def evaluate2(prob):
+    op = '+'
+    counter = 0
+    c = next(prob, 'end')
+    while(c != 'end' ):
+        if(c in ['+', '*']):
+            op = c 
+        elif(c == '('):
+            v = evaluate2(prob)
+        elif(c == ')' or c == '*'):
+            return counter
+        else:
+            v = int(c)
+        if(c in ['+']):
+            op = c
+        elif(c == '*'):
+            #counter = counter * evaluate2(prob)
+            return counter * evaluate2(prob)
+        else:
+            counter = counter + v
+        c = next(prob, 'end')
+    return counter
 
 if __name__ == "__main__":
     s = '6 + ((7 * 2) + 4)'
     with open('input.txt', 'r') as f:
-        total = 0
+        total1 = 0
+        total2 = 0
         for s in f:
-            prob = iter([a for a in list(s.strip()) if a != ' '])
-            total = total + evaluate(prob)
-        print(total)
+            prob1 = iter([a for a in list(s.strip()) if a != ' '])
+            prob2  = iter([a for a in list(s.strip()) if a != ' '])
+            total1 = total1 + evaluate(prob1)
+            total2 = total2 + evaluate2(prob2)
+        print(total1)
+        print(total2)
     #prob = iter([a for a in list(s) if a != ' '])
     #print(evaluate(prob))
